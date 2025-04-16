5ac82e0813a71efd9748f3851cec8585d35288f7
Aryan Neupane <official.aryanneupane@gmail.com>
2025-04-16 23:57:21 +0545

    initial react setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='w-full h-screen bg-zinc-700 p-10'>
+      <div className='w-44 h-32 rounded-xl bg-red-600'> </div>
+    </div>
+  )
+}
+
+export default App
