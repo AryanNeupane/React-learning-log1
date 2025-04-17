4ba4a23ebe7a185af9ab2c4916ecd17f10dbbccc
Aryan Neupane <official.aryanneupane@gmail.com>
2025-04-17 10:35:58 +0545

    state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Product from './Product'
 
 function App() {
+  let [a,b]=useState(10)
   return (
-    <div className='w-full h-screen bg-zinc-700 p-10'>
-      <div className='w-44 h-32 rounded-xl bg-red-600'> </div>
+    // <div className='w-full h-screen bg-zinc-700 p-10'>
+    //   <div className='w-44 h-32 rounded-xl bg-red-600'> </div>
+    //   <Product/>
+    // </div>
+    <div className='w-full h-screen bg-zinc-700 text-white'>
+      <h1>{a}</h1>
+      <button onClick={()=>b(a+1)} className='px-3 py-1 rounded-md bg-green-500 text-black'>Click</button>
+      <Product item={a} />
+
     </div>
   )
 }
---
+++ src/Product.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function Product({item}) {
+  return (
+    <div className='text-white w-full h-96 bg-zinc-500'>{item}</div>
+  )
+}
+
+export default Product
